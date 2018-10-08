524f9700bb0e62a1a1cf1c410c27510943f08b9d
殷煌 <huang.yin@aihuishou.com>
2018-10-08 17:40:48 +0800

    update

--- src/components/alert/index.tsx
+++ src/components/alert/index.tsx
@@ -1,21 +1,67 @@
 // 弹筐组件
 import * as React from 'react';
+import ReactDOM from 'react-dom';
+import { Button } from 'antd-mobile';
 import './index.less';
 
-export default class Alert extends React.Component<any, any>
-{
-    public render()
+interface Iprops {
+    onConfirm: () => void,
+    btnText: string,
+    message: string,
+    title: string
+}
+
+export default (title: string, message: string, btnText: string, onConfirm: () => void) => {
+
+    // 释放组件
+    const disposeNode = () => {
+        ReactDOM.unmountComponentAtNode(div);
+        if (div.parentNode) {
+            div.parentNode.removeChild(div);
+        }
+    };
+
+    // 插入message提示的容器至body
+    const div = document.createElement('div');
+    document.body.appendChild(div);
+
+
+    class Alert extends React.Component<Iprops>
     {
-        return (
-            <div className="alert-wrapper">
-                <div className="alert-box">
-                    <div className="alert-content">
-                        {/* 存放内容 */}
-                        {this.props.children}
+        public onClose = () => {
+            disposeNode();
+        }
+        public onConfirm = () => {
+            if (this.props.onConfirm) {
+                this.props.onConfirm();
+                disposeNode();
+            }
+        }
+        public render() {
+            return (
+                <div className="comp-alert-container">
+                    <div className="mask" onClick={this.onClose} />
+                    <div className="alert-wrapper">
+                        <img src={require('@/img/right1.png')} className="alert-success-icon" />
+                        <div className="alert-title" dangerouslySetInnerHTML={{ '__html': this.props.title }} />
+                        <div className="alert-content" dangerouslySetInnerHTML={{ '__html': this.props.message }} />
+                        <div className="alert-button-group">
+                            <Button
+                                type="primary"
+                                onClick={this.onConfirm}
+                            >{this.props.btnText}</Button>
+                        </div>
                     </div>
                 </div>
-                
-            </div>
-        );
+            );
+        }
     }
+
+    // message
+    const node = (
+        <Alert title={title} message={message} btnText={btnText} onConfirm={onConfirm} />
+    );
+
+    // 渲染message
+    ReactDOM.render(node, div);
 }
--- src/containers/home/auction.tsx
+++ src/containers/home/auction.tsx
@@ -3,10 +3,11 @@
  * nns主页下的域名竞拍模块
  */
 import * as React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { observer, inject } from 'mobx-react';
 import './index.less'
 import Input from '@/components/Input/Input'
+import Alert from '@/components/alert';
 import TitleText from '@/components/titletext';
 import { Button, WingBlank } from 'antd-mobile';
 // 引入数据类型
@@ -22,10 +23,10 @@ class Auction extends React.Component<IHomeProps>
   public prop = this.props.intl.messages;
   public componentDidMount() {
     this.props.home.messages = {
-      errmsg1:this.prop.home.auction.errmsg1,
-      errmsg2:this.prop.home.auction.errmsg2,
-      successmsg:this.prop.home.auction.successmsg,
-      successmsg2:this.prop.home.auction.successmsg2
+      errmsg1: this.prop.home.auction.errmsg1,
+      errmsg2: this.prop.home.auction.errmsg2,
+      successmsg: this.prop.home.auction.successmsg,
+      successmsg2: this.prop.home.auction.successmsg2
     }
   }
   public change = (value: string) => {
@@ -33,8 +34,11 @@ class Auction extends React.Component<IHomeProps>
   }
   public onStartAuction = async () => {
     // todo
-		await this.props.common.sendrawtransaction('8000000149f2db1d71febefe9d12e6e840e988a28ab516134b38e948f8051ea0cc0f22cb010002e72d286979ee6cb1b7e65dfddfb2e384100b8d148e7758de42e4168b71792c6040420f00000000008c8ed58be92fd1b01896dd9e02acd45776eb3e8ce72d286979ee6cb1b7e65dfddfb2e384100b8d148e7758de42e4168b71792c60203e57d4e8000000b7b0b2d92b970affd9567faf10205ca222e389330141406ca464fdaab4a63d4bf2737c2035944fcef8511caaa59aa0bdf73107dd93825b66344b107cf6b9c13aa9baa6749217541d94d56e54f2cc9a6bc54e0da36c6c6e2321029020fd0914c677a1ab7d8b3502ca9a4506efbf7572ea24fb3862f48dd153ee5bac');
-    alert('操作成功');
+    await this.props.common.sendrawtransaction('8000000149f2db1d71febefe9d12e6e840e988a28ab516134b38e948f8051ea0cc0f22cb010002e72d286979ee6cb1b7e65dfddfb2e384100b8d148e7758de42e4168b71792c6040420f00000000008c8ed58be92fd1b01896dd9e02acd45776eb3e8ce72d286979ee6cb1b7e65dfddfb2e384100b8d148e7758de42e4168b71792c60203e57d4e8000000b7b0b2d92b970affd9567faf10205ca222e389330141406ca464fdaab4a63d4bf2737c2035944fcef8511caaa59aa0bdf73107dd93825b66344b107cf6b9c13aa9baa6749217541d94d56e54f2cc9a6bc54e0da36c6c6e2321029020fd0914c677a1ab7d8b3502ca9a4506efbf7572ea24fb3862f48dd153ee5bac');
+
+    Alert(this.prop.message.successmsg, this.prop.message.waitmsg, this.prop.btn.confirm, function () {
+      alert('我点击了确认按钮')
+    });
   }
   public render() {
     return (
@@ -54,17 +58,17 @@ class Auction extends React.Component<IHomeProps>
           <span className="auction-neo">.neo</span>
         </div>
         {
-          this.props.home.inputModule.status !== 'error' && 
-           <WingBlank>
-              {
-                this.props.home.isStatus === 1 &&
-                <Button type="primary" onClick={this.onStartAuction}>{this.prop.btn.startauction}</Button>
-              }
-              {
-                this.props.home.isStatus === 2 &&
-                <Link to="/auction"><Button type="primary">{this.prop.btn.join}</Button></Link>
-              }
-            </WingBlank>
+          this.props.home.inputModule.status !== 'error' &&
+          <WingBlank>
+            {
+              this.props.home.isStatus === 1 &&
+              <Button type="primary" onClick={this.onStartAuction}>{this.prop.btn.startauction}</Button>
+            }
+            {
+              this.props.home.isStatus === 2 &&
+              <Link to="/auction"><Button type="primary">{this.prop.btn.join}</Button></Link>
+            }
+          </WingBlank>
         }
       </div>
     );
